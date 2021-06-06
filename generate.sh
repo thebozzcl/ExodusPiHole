701cde4fd2216781401414c9ece533f5bd7c7036
Rodrigo Bozzolo <rjbozzol@uc.cl>
2021-06-06 10:52:45 -0700

    Updated to use raw Exodus data from JSON

--- generate.sh
+++ generate.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-TRACKER_LIST_URL=https://reports.exodus-privacy.eu.org/en/trackers/
+TRACKER_LIST_URL=https://etip.exodus-privacy.eu.org/trackers/export
 OUTPUT_PATH=./list/exodus-pihole.txt
 NO_COMMENT_PATH=./list/exodus-pihole.no-comment.txt
 
 echo "# Generated on `date +'%D %T'`" > $OUTPUT_PATH
 
-for tracker in `curl -s "$TRACKER_LIST_URL" | grep -E "/en/trackers/[0-9]{1,}/" | cut -d '/' -f4`
+curl -s $TRACKER_LIST_URL | jq '.trackers[]' -c | while IFS= read -r tracker
 do
-	url=$TRACKER_LIST_URL$tracker/
-	echo "# $url"
+	name=`echo $tracker | jq '.name' -r`
+        echo $name
+	echo "# $name" >> $OUTPUT_PATH
 
-	rule=`curl -s "$url" | grep "Network detection rule" | sed "s/.*<code>//g" | sed "s/<\/code>//g" | sed "s/|//g"`
-	if [[ $rule == *"NC"* ]] || [[ $rule == *"404"* ]]
-	then
-		echo "# No rule found"
-		continue
-	fi
-
-	for match in $rule; do
-		echo $match
-	done
-
-	sleep 1
-done >> $OUTPUT_PATH
+	rule=`IFS=';' echo $tracker | jq '.network_signature' -r`
+        if [[ $rule = "" ]]
+        then
+                continue
+        fi
+        echo $rule 
+        echo $rule >> $OUTPUT_PATH
+done
 
 cat $OUTPUT_PATH | grep -v "#" > $NO_COMMENT_PATH
