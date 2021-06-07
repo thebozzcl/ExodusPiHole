e336071a37f4b7b6d9866bdc3e45851e445b013a
Rodrigo Bozzolo <rjbozzol@uc.cl>
2021-06-06 19:44:05 -0700

    Added update script

---
+++ update-exodus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for domain in `curl -s https://raw.githubusercontent.com/thebozzcl/ExodusPiHole/main/list/exodus-pihole.no-comment.txt`;
+do 
+    pihole --regex $domain;
+    sleep 0.5; # I noticed on my first run that some entries were skipped because of a database lock. This mitigates that.
+done
