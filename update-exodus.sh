f78e1df376429c31cfce2ffccc25f50fc1584f38
Rodrigo Bozzolo <rjbozzol@uc.cl>
2022-01-30 16:39:33 -0800

    The update-exodus.sh script can now take a URL as an argument

--- update-exodus.sh
+++ update-exodus.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DEFAULT_LIST_URL=https://raw.githubusercontent.com/thebozzcl/ExodusPiHole/main/list/exodus-pihole.txt
+
+regex_list_url=${1:-$DEFAULT_LIST_URL}
+
 for domain in `curl -s https://raw.githubusercontent.com/thebozzcl/ExodusPiHole/main/list/exodus-pihole.txt`;
 do 
     pihole --regex $domain;
